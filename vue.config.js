81d8c968c1c3d99cdc826e7f60c0e0d3d3f9724d
Kenneth Aamås <kenniii90@gmail.com>
2019-01-30 15:57:53 +0800

    Added build script. Removing generated html files (index and pages). Extracted css to root of /dist. Including /dist in git, but ignoring the /js folder. Added npm files for *.css.

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,38 @@
+const pages = {
+    kSpinnerCherry: {
+        entry: 'src/KSpinnerCherry.vue'
+    },
+    kSpinnerPeach: {
+        entry: 'src/KSpinnerPeach.vue'
+    }
+}
+
 module.exports = {
     chainWebpack: config => {
+        // Prevent html generation of index
         config.plugins.delete('html')
         config.plugins.delete('preload')
         config.plugins.delete('prefetch')
-        config.optimization.delete('splitChunks')
+
+        // Prevent html generation of pages
+        Object.keys(pages).forEach(page => {
+            config.plugins.delete(`html-${ page }`)
+            config.plugins.delete(`preload-${ page }`)
+            config.plugins.delete(`prefetch-${ page }`)
+        })
+    },
+    configureWebpack: {
+        optimization: {
+            splitChunks: false
+        }
+    },
+    css: {
+        extract: {
+            filename: '[name].css',
+            chunkFilename: '[name].css',
+        }
     },
     filenameHashing: false,
-    pages: {
-        kSpinnerCherry: {
-            entry: 'src/KSpinnerCherry.vue',
-            filename: 'kSpinnerCherry.css',
-            chunks: []
-        },
-        kSpinnerPeach: {
-            entry: 'src/KSpinnerPeach.vue',
-            filename: 'kSpinnerPeach.css',
-            chunks: []
-        }
-    }
+    pages,
+    productionSourceMap: false
 }
